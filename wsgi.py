fd7b2ce744cdf581190e7ad2369112a614af364a
Rahman Taufik <rt@Rahmans-MacBook-Pro.local>
2024-06-08 22:33:27 +0700

    update static and template

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,7 @@ today = date.today()
 
 @app.route('/')
 def main():
-    return 'hello mother father'
+    return render_template('index.html')
 
 @app.route('/lppmunila_year')
 def extract_lppmunila_year():
@@ -26,8 +26,23 @@ def extract_sinta_univ():
     sinta_univ()
     return 'Done'
 
-@app.route('/sinta_author')
-def extract_sinta_author():
+@app.route('/sinta_author_base', methods=['POST', 'GET'])
+def sinta_author_base():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        id_sinta = request.form['id_sinta']
+        return redirect(url_for('sinta_author_extract', uname=username, pw=password, ids=id_sinta))
+    else:
+        return render_template('sinta.html')
+
+@app.route('/sinta_author_extract/<uname>/<pw>/<ids>')
+def sinta_author_extract(uname=None, pw=None, ids=None):
+    print(uname)
+    print(pw)
+    print(ids)
+    # return "haha"
+    # total_data, df = sinta_author(uname, pw, ids)
     total_data, df = sinta_author()
     print(total_data)
     return send_file(
